3e5b4a1b1a931ec7e8456baaa44ba6760fc77430
achlan2 <achlan.nurachman@gmail.com>
2025-09-23 08:23:20 +0700

    feat: parentNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -19,11 +19,27 @@ class HTMLNode():
 
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
-        super().__init__(tag, value, props)
+        super().__init__(tag, value, None, props)
 
     def to_html(self):
         if not self.value:
-            raise ValueError
+            raise ValueError('value is empty!')
         if not self.tag:
             return self.value;
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if not self.tag:
+            raise ValueError('tag is empty!')
+        if not self.children:
+            raise ValueError('Should have at least 1 children!')
+        res = f"<{self.tag}{self.props_to_html()}>"
+        for child in self.children:
+            res+= child.to_html()
+        res+=f"</{self.tag}>"
+        return res
+
