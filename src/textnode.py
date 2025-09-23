1bf5eec11d31b99a0afaf71e64ce9557bfb6dfef
achlan2 <achlan.nurachman@gmail.com>
2025-09-23 08:32:28 +0700

    feat: text to htmlnode

--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,5 @@
 from enum import Enum
-
+from htmlnode import HTMLNode, LeafNode
 
 class TextType(Enum):
     TEXT = "text"
@@ -23,4 +23,23 @@ class TextNode():
             and self.url == other.url
         )
     def __repr__(self):
-        return f"TextNode({self.text}, {self.text_type}, {self.url})"
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
+def text_node_to_html_node(text_node):
+    type = text_node.text_type
+    match(type):
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode('b', text_node.text)
+        case TextType.ITALIC:
+            return LeafNode('i', text_node.text)
+        case TextType.CODE:
+            return LeafNode('code', text_node.text)
+        case TextType.LINK:
+            return LeafNode('a', text_node.text, { 'href': text_node.url })
+        case TextType.IMAGE:
+            return LeafNode('img', '', { 'src': text_node.url, 'alt': text_node.text })
+        case _:
+            raise ValueError('Unexpected TYPE!')
